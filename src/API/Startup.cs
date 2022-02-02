9dacf37cdae502101c7b835548fe93e9f22f9444
Dmitry925 <rogowdmitriy917@gmail.com>
2022-02-02 09:32:19 +0300

    Added startup class

---
+++ src/API/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace API
+{
+    public static class Startup
+    {
+        public static WebApplication InitializeApp(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder);
+
+            var app = builder.Build();
+            Configure(app);
+
+            return app;
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen();
+        }
+
+        private static void Configure(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
