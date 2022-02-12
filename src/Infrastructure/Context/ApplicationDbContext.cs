1c7a45c537334198ace389330e389a64b899ef29
Dmitry925 <rogowdmitriy917@gmail.com>
2022-02-12 15:50:37 +0300

    Added ApplicationDbContext

---
+++ src/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Map> Maps { get; set; }
+        public DbSet<Office> Office { get; set; }
+        public DbSet<ParkingPlace> ParkingPlaces { get; set; }
+        public DbSet<Vacation> Vacations { get; set; }
+        public DbSet<WorkPlace> WorkPlaces { get; set; }
+
+        public async Task<int> SaveChanges()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
