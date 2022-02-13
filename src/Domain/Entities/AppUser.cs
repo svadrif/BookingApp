af0cfc0601578f3224c01318a58c30adf7a283b8
temshik <97480026+temshik@users.noreply.github.com>
2022-02-13 16:08:39 +0300

    Deleted link to Workplace
    Removed link between PrefferdWorkPlaceId and WorkPlace.Id

--- src/Domain/Entities/AppUser.cs
+++ src/Domain/Entities/AppUser.cs
@@ -18,13 +18,11 @@ namespace Domain.Entities
         public Roles Role { get; set; }
         public DateTime EmploymentStart { get; set; }
         public DateTime? EmploymentEnd { get; set; }
+        public Guid? PrefferdWorkPlaceId { get; set; }
         public bool isDeleted { get; set; }
 
         /* EF Relation */
         public IEnumerable<Vacation> Vacations { get; set; }
         public IEnumerable<Booking> Bookings { get; set; }
-
-        public Guid? PrefferdWorkPlaceId { get; set; }
-        public WorkPlace WorkPlace { get; set; }
     }
 }
