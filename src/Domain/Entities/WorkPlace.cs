56e10a058e6193524d10433a4de3ab5a85eeda82
temshik <97480026+temshik@users.noreply.github.com>
2022-02-14 07:08:08 +0300

    Deleted link to AppUser
    Removed link between Id and AppUser.PrefferdWorkPlaceId

--- src/Domain/Entities/WorkPlace.cs
+++ src/Domain/Entities/WorkPlace.cs
@@ -19,7 +19,6 @@ namespace Domain.Entities
 
         /* EF Relation */
         public IEnumerable<Booking> Bookings { get; set; }
-        public IEnumerable<AppUser> Users { get; set; }
       
         public Guid MapId { get; set; }
         public Map Map { get; set; }
