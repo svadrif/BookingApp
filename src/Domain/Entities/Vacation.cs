001d4203c90cff40338a6b5cac8e9de7698881de
vnu2305 <70842872+vnu2305@users.noreply.github.com>
2022-02-18 14:03:39 +0500

    Update Vacation.cs

--- src/Domain/Entities/Vacation.cs
+++ src/Domain/Entities/Vacation.cs
@@ -11,7 +11,6 @@ namespace Domain.Entities
         public DateTime? VacationEnd { get; set; }
 
         /* EF Relation */
-        [JsonIgnore]
         public AppUser User { get; set; }
         public Guid UserId { get; set; }
     }
