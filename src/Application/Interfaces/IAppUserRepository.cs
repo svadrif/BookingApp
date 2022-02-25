64c68d87f5d5c670c4e35643fa178ec4b27dff81
vasiliv <ggo24@yahoo.com>
2022-02-25 15:45:41 +0400

    Corrected bugs

--- src/Application/Interfaces/IAppUserRepository.cs
+++ src/Application/Interfaces/IAppUserRepository.cs
@@ -8,11 +8,7 @@ using System.Threading.Tasks;
 namespace Application.Interfaces
 {
     public interface IAppUserRepository : IGenericRepository<AppUser>
-    {   
-        // for override
-        new Task<List<AppUser>> GetAll();
-        new Task<AppUser> GetById(Guid id);
-        
+    {                
         Task<IEnumerable<AppUser>> SearchAppUser(string searchedValue);
     }
 }
