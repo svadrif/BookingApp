fbd5950285ecba6aec28bad971bb8c1295376d19
temshik <97480026+temshik@users.noreply.github.com>
2022-02-28 09:27:34 +0300

    Delete AddOfficeDTO.cs
    it would be in my PR

--- src/Application/DTOs/OfficeDTO/AddOfficeDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.DTOs.OfficeDTO;
-
-public class AddOfficeDTO
-{
-    public string Country { get; set; }
-    public string City { get; set; }
-    public string Address { get; set; }
-    public string Name { get; set; }
-}
