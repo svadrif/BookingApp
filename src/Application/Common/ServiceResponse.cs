043dc5863bb91474abe9bcfebcfb820da7664356
temshik <97480026+temshik@users.noreply.github.com>
2022-02-28 09:26:05 +0300

    Delete ServiceResponse.cs
    it dose not need

--- src/Application/Common/ServiceResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Application.Common;
-
-public class ServiceResponse<T>
-{
-    public T Data { get; set; }
-    public bool success { get; set; } = true;
-    public string? message { get; set; } = null;
-}
