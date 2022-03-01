26dc6f1628e97d169c56c5b64093eb80ba8c6cf6
vasiliv <ggo24@yahoo.com>
2022-03-01 19:43:17 +0400

    Deleted IDisposable from IGenericRepository

--- src/Application/Interfaces/IGenericRepository.cs
+++ src/Application/Interfaces/IGenericRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
-    public interface IGenericRepository<T>: IDisposable where T : class
+    public interface IGenericRepository<T> where T : class
     {
         Task AddAsync(T entity);
         Task<List<T>> GetAllAsync();
