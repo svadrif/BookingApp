371e663d7ae16b967f75797ddcd6467d3e875166
Dmitry925 <67834974+Dmitry925@users.noreply.github.com>
2022-03-02 12:29:16 +0300

    Update PagedBase.cs

--- src/Application/Pagination/PagedBase.cs
+++ src/Application/Pagination/PagedBase.cs
@@ -5,5 +5,5 @@ public abstract class PagedBase
     public int PageSize { get; set; } = 10;
     public int CurrentPage { get; set; } = 1;
 
-    public int SkipCount => (CurrentPage - 1) * PageSize;
+    public int SkipCount() => (CurrentPage - 1) * PageSize;
 }
