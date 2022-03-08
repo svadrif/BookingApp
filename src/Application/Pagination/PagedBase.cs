d82325d078699b9c7a127b31f49922dd61e4d761
Dmitry925 <rogowdmitriy917@gmail.com>
2022-03-08 15:43:40 +0300

    Changed pagination

--- src/Application/Pagination/PagedBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.Pagination;
-
-public abstract class PagedBase
-{
-    public int PageSize { get; set; } = 10;
-    public int CurrentPage { get; set; } = 1;
-
-    public int SkipCount() => (CurrentPage - 1) * PageSize;
-}
--- src/Application/Pagination/PagedQueryBase.cs
+++ src/Application/Pagination/PagedQueryBase.cs
@@ -1,7 +1,11 @@
 ﻿namespace Application.Pagination;
 
-public class PagedQueryBase : PagedBase
+public class PagedQueryBase
 {
+    public int PageSize { get; set; } = 10;
+    public int CurrentPage { get; set; } = 1;
     public string SortOn { get; set; }
     public string SortDirection { get; set; }
+
+    public int SkipCount() => (CurrentPage - 1) * PageSize;
 }
