26c0d85a2c355bee75f4f5f8b414de3dba646d09
Dmitry925 <rogowdmitriy917@gmail.com>
2022-03-14 18:59:04 +0300

    Added sorting method to QueryableExtentions

--- src/Application/Extentions/QueryableExtensions.cs
+++ src/Application/Extentions/QueryableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Pagination;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Application.Extentions;
 
@@ -12,5 +13,21 @@ public static class QueryableExtentions
         var items = await source.Skip(query.SkipCount()).Take(query.PageSize).ToListAsync();
 
         return new PagedList<TEntity>(items, count, query.CurrentPage, query.PageSize);
+    }
+
+    public static IOrderedQueryable<TEntity> Sort<TEntity>(this IQueryable<TEntity> source, string sortBy, string sortDirection)
+        where TEntity : class
+    {
+        return sortDirection.ToUpper() == "ASC" ? source.OrderBy(ToLambda<TEntity>(sortBy))
+                                                : source.OrderByDescending(ToLambda<TEntity>(sortBy));
+    }
+
+    private static Expression<Func<T, object>> ToLambda<T>(string propertyname)
+    {
+        var parameter = Expression.Parameter(typeof(T));
+        var property = Expression.Property(parameter, propertyname);
+        var propAsObject = Expression.Convert(property, typeof(object));
+
+        return Expression.Lambda<Func<T, object>>(propAsObject, parameter);
     }
 }
