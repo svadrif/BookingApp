94046d1d7c5555dd0c99c320e999abe2d6680af9
Dmitry925 <rogowdmitriy917@gmail.com>
2022-03-19 22:34:04 +0300

    Added StartCommand

---
+++ src/TelegramBot/Commands/StartCommand.cs
@@ -0,0 +1,24 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using TelegramBot.Helpers;
+
+namespace TelegramBot.Commands
+{
+    public static class StartCommand
+    {
+        public static async Task ExecuteAsync(Message message, ITelegramBotClient botClient)
+        {
+            var buttons = new List<InlineKeyboardButton>{
+                InlineKeyboardButton.WithCallbackData("New booking", "New Booking"),
+                InlineKeyboardButton.WithCallbackData("My bookings", "My Bookings")
+            };
+
+            var inlineKeyboard = KeyboardBuilder.BuildInLineKeyboard(buttons, 2);
+
+            await botClient.SendTextMessageAsync(chatId: message.From.Id,
+                                                 text: "Select action:",
+                                                 replyMarkup: inlineKeyboard);
+        }
+    }
+}
