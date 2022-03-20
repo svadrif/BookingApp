860d52ffb42d1e8f395cd28d157e75b0ee7e6eaa
Dmitry925 <rogowdmitriy917@gmail.com>
2022-03-20 12:31:46 +0300

    Added StateRepository

---
+++ src/Infrastructure/Repositories/StateRepository.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces.IRepositories;
+using Domain.Entities;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class StateRepository : GenericRepository<State>, IStateRepository
+    {
+        public StateRepository(ApplicationDbContext context) : base(context) { }
+
+        public async Task<State> GetByUserIdAsync(Guid id, bool tracking = false)
+        {
+            return await Search(x => x.UserId == id,
+                                tracking)
+                        .FirstOrDefaultAsync();
+        }
+    }
+}
--- src/Infrastructure/Repositories/UnitOfWork.cs
+++ src/Infrastructure/Repositories/UnitOfWork.cs
@@ -13,6 +13,7 @@ namespace Infrastructure.Repositories
         public IParkingPlaceRepository ParkingPlaces { get; private set; }
         public IVacationRepository Vacations { get; private set; }
         public IWorkPlaceRepository WorkPlaces { get; private set; }
+        public IStateRepository States { get; private set; }
 
         public UnitOfWork(IApplicationDbContext context,
             IAppUserRepository appUsers,
@@ -21,7 +22,8 @@ namespace Infrastructure.Repositories
             IBookingRepository bookings,
             IParkingPlaceRepository parkingPlaces,
             IVacationRepository vacations,
-            IWorkPlaceRepository workPlaces)
+            IWorkPlaceRepository workPlaces, 
+            IStateRepository states)
         {
             _context = context;
             AppUsers = appUsers;
@@ -31,6 +33,7 @@ namespace Infrastructure.Repositories
             ParkingPlaces = parkingPlaces;
             Vacations = vacations;
             WorkPlaces = workPlaces;
+            States = states;
         }
 
         public async Task<int> CompleteAsync(CancellationToken cancellationToken = new CancellationToken())
