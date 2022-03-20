33cb2a31c209953146298da526ed438916e416a4
Dmitry925 <rogowdmitriy917@gmail.com>
2022-03-20 16:05:28 +0300

    Added repository and service registration

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -26,6 +26,7 @@ namespace Infrastructure
             services.AddScoped<IParkingPlaceRepository, ParkingPlaceRepository>();
             services.AddScoped<IVacationRepository, VacationRepository>();
             services.AddScoped<IWorkPlaceRepository, WorkPlaceRepository>();
+            services.AddScoped<IStateRepository, StateRepository>();
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             #endregion
@@ -45,6 +46,7 @@ namespace Infrastructure
             services.AddScoped(typeof(IMapService), typeof(MapService));
             services.AddScoped(typeof(IOfficeService), typeof(OfficeService));
             services.AddScoped(typeof(IParkingPlaceService), typeof(ParkingPlaceService));
+            services.AddScoped(typeof(IStateService), typeof(StateService));
             services.AddTransient<IAuthenticationService, AuthenticationService>();
             #endregion
 
