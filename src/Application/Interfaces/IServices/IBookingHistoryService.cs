328d76befc4d51b737de3c7b0f1920950a719d9b
Dmitry925 <rogowdmitriy917@gmail.com>
2022-03-20 16:55:35 +0300

    Added BookingHistoryService

---
+++ src/Application/Interfaces/IServices/IBookingHistoryService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces.IServices
+{
+    public interface IBookingHistoryService
+    {
+        Task AddAsync(BookingHistory bookingHistory);
+        Task<BookingHistory> GetByUserIdAsync(Guid userId, bool tracking = false);
+        Task<bool> RemoveByUserIdAsync(Guid userId);
+        Task<BookingHistory> UpdateAsync(BookingHistory bookingHistory);
+    }
+}
