04202cc93128fbfa284f5df4392b814f36f9480f
Dmitry925 <rogowdmitriy917@gmail.com>
2022-03-21 16:22:22 +0300

    Added SendCountriesCommand

---
+++ src/TelegramBot/Commands/SendCountriesCommand.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces.IServices;
+using Application.Pagination;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using TelegramBot.Helpers;
+
+namespace TelegramBot.Commands
+{
+    public static class SendCountriesCommand
+    {
+        public static async Task ExecuteAsync(CallbackQuery callback, ITelegramBotClient botClient, IOfficeService officeService)
+        {
+            var countries = await officeService.GetCountriesAsync(new PagedQueryBase());
+            var buttons = new List<InlineKeyboardButton>();
+            foreach (var country in countries)
+            {
+                buttons.Add(InlineKeyboardButton.WithCallbackData(country, country));
+            }
+
+            var inlineKeyboard = KeyboardBuilder.BuildInLineKeyboard(buttons, 2);
+            await botClient.EditMessageTextAsync(chatId: callback.From.Id,
+                                                 messageId: callback.Message.MessageId,
+                                                 text: "Select country:",
+                                                 replyMarkup: inlineKeyboard);
+        }
+    }
+}
