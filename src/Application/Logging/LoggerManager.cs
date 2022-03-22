7e0310b85cf3c2d486ca305647038e2d98348138
vasiliv <ggo24@yahoo.com>
2022-03-22 20:45:37 +0400

    LoggerManager Location changed

--- src/Application/Logging/LoggerManager.cs
+++ src/Application/Logging/LoggerManager.cs
@@ -1,9 +1,12 @@
 ﻿using Application.Interfaces;
 using Serilog;
-using Serilog.Core;
-using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace API
+namespace Application.Logging
 {
     public class LoggerManager : ILoggerManager
     {
