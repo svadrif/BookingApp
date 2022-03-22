4a3ea1b13ddac5848f12e9f44041b25ec61a6d63
vasiliv <ggo24@yahoo.com>
2022-03-22 21:37:56 +0400

    Logging added to MapRepository

--- src/Infrastructure/Repositories/MapRepository.cs
+++ src/Infrastructure/Repositories/MapRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Extentions;
+using Application.Interfaces;
 using Application.Interfaces.IRepositories;
 using Application.Pagination;
 using Domain.Entities;
@@ -8,30 +9,58 @@ namespace Infrastructure.Repositories
 {
     public class MapRepository : GenericRepository<Map>, IMapRepository
     {
-        public MapRepository(ApplicationDbContext context) : base(context) { }
+        private readonly ILoggerManager _logger;
+        public MapRepository(ApplicationDbContext context, ILoggerManager logger) : base(context)
+        {
+            _logger = logger;
+        }
 
         public async Task<PagedList<Map>> GetPagedAsync(PagedQueryBase query, bool tracking = false)
         {
+            try
+            { 
             return await GetAll(tracking)
                         .Sort(query.SortOn, query.SortDirection)
                         .ToPagedListAsync(query);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetPagedAsync)}action {ex}");
+                return null;
+            }
         }
 
         public async Task<PagedList<Map>> GetPagedByAttributesAsync(bool hasKitchen, bool hasConfRoom, PagedQueryBase query, bool tracking = false)
         {
-            return await Search(x => x.HasKitchen == hasKitchen
+            try
+            {
+                return await Search(x => x.HasKitchen == hasKitchen
                                 && x.HasConfRoom == hasConfRoom,
                                 tracking)
                         .Sort(query.SortOn, query.SortDirection)
                         .ToPagedListAsync(query);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetPagedByAttributesAsync)}action {ex}");
+                return null;
+            }
         }
 
         public async Task<PagedList<Map>> GetPagedByOfficeIdAsync(Guid officeId, PagedQueryBase query, bool tracking = false)
         {
-            return await Search(x => x.OfficeId == officeId,
+            try
+            {
+                return await Search(x => x.OfficeId == officeId,
                                 tracking)
                         .Sort(query.SortOn, query.SortDirection)
                         .ToPagedListAsync(query);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetPagedByOfficeIdAsync)}action {ex}");
+                return null;
+            }
         }
     }
 }
