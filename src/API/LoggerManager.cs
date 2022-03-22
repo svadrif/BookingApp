74bee2766ba42b27da7a5555071e11a2a2d48926
vasiliv <ggo24@yahoo.com>
2022-03-22 19:24:38 +0400

    LoggerManager and ILoggerManager added

---
+++ src/API/LoggerManager.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace API
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static Serilog.ILogger logger = Log.Logger;
+
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+        public void LogInfo(string message)
+        {
+            logger.Information(message);
+        }
+        public void LogWarn(string message)
+        {
+            logger.Warning(message);
+        }
+    }
+}
