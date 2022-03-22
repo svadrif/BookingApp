01e4cf3e730e38457bfb9d19a4665bc33ae87941
vasiliv <ggo24@yahoo.com>
2022-03-22 22:33:11 +0400

    Logging added to StateRepository

--- src/Infrastructure/Repositories/StateRepository.cs
+++ src/Infrastructure/Repositories/StateRepository.cs
@@ -1,4 +1,5 @@
-﻿using Application.Interfaces.IRepositories;
+﻿using Application.Interfaces;
+using Application.Interfaces.IRepositories;
 using Domain.Entities;
 using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
@@ -7,13 +8,25 @@ namespace Infrastructure.Repositories
 {
     public class StateRepository : GenericRepository<State>, IStateRepository
     {
-        public StateRepository(ApplicationDbContext context) : base(context) { }
+        private readonly ILoggerManager _logger;
+        public StateRepository(ApplicationDbContext context, ILoggerManager logger) : base(context)
+        {
+            _logger = logger;
+        }
 
         public async Task<State> GetByUserIdAsync(Guid userId, bool tracking = false)
         {
-            return await Search(x => x.UserId == userId,
+            try
+            {
+                return await Search(x => x.UserId == userId,
                                 tracking)
                         .FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetByUserIdAsync)}action {ex}");
+                return null;
+            }
         }
     }
 }
