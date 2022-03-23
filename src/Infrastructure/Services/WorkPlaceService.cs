a32b804b6f99aeb470052f9047f526dcbe26e867
vasiliv <ggo24@yahoo.com>
2022-03-24 00:33:30 +0400

    Logging added to services

--- src/Infrastructure/Services/WorkPlaceService.cs
+++ src/Infrastructure/Services/WorkPlaceService.cs
@@ -4,6 +4,7 @@ using Application.Interfaces.IServices;
 using Application.Pagination;
 using AutoMapper;
 using Domain.Entities;
+using Application.Interfaces;
 
 namespace Infrastructure.Services
 {
@@ -11,63 +12,113 @@ namespace Infrastructure.Services
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-        public WorkPlaceService(IUnitOfWork unitOfWork, IMapper mapper)
+        private readonly ILoggerManager _logger;
+        public WorkPlaceService(IUnitOfWork unitOfWork, IMapper mapper, ILoggerManager logger)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _logger = logger;
         }
 
         public async Task<Guid> AddAsync(AddWorkPlaceDTO workPlaceDTO)
         {
-            WorkPlace workPlace = _mapper.Map<WorkPlace>(workPlaceDTO);
-            await _unitOfWork.WorkPlaces.AddAsync(workPlace);
-            await _unitOfWork.CompleteAsync();
-            return workPlace.Id;
+            try
+            {
+                WorkPlace workPlace = _mapper.Map<WorkPlace>(workPlaceDTO);
+                await _unitOfWork.WorkPlaces.AddAsync(workPlace);
+                await _unitOfWork.CompleteAsync();
+                return workPlace.Id;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarn($"Non correct values in the {nameof(AddAsync)} action {ex}");
+                return Guid.Empty;
+            }
         }
 
         public async Task<PagedList<GetWorkPlaceDTO>> GetPagedAsync(PagedQueryBase query)
         {
-            var workPlaces = await _unitOfWork.WorkPlaces.GetPagedAsync(query);
-            var mapWorkPlaces = _mapper.Map<List<GetWorkPlaceDTO>>(workPlaces);
-            var workPlacesDTO = new PagedList<GetWorkPlaceDTO>(mapWorkPlaces, workPlaces.TotalCount, workPlaces.CurrentPage, workPlaces.PageSize);
-            return workPlacesDTO;
+            try
+            {
+                var workPlaces = await _unitOfWork.WorkPlaces.GetPagedAsync(query);
+                var mapWorkPlaces = _mapper.Map<List<GetWorkPlaceDTO>>(workPlaces);
+                var workPlacesDTO = new PagedList<GetWorkPlaceDTO>(mapWorkPlaces, workPlaces.TotalCount, workPlaces.CurrentPage, workPlaces.PageSize);
+                return workPlacesDTO;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarn($"Non correct values in the {nameof(GetPagedAsync)} action {ex}");
+                return null;
+            }
         }
 
         public async Task<GetWorkPlaceDTO> GetByIdAsync(Guid Id)
         {
-            var workPlace = await _unitOfWork.WorkPlaces.GetByIdAsync(Id);
-            return _mapper.Map<GetWorkPlaceDTO>(workPlace);
+            try
+            {
+                var workPlace = await _unitOfWork.WorkPlaces.GetByIdAsync(Id);
+                return _mapper.Map<GetWorkPlaceDTO>(workPlace);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarn($"Non correct values in the {nameof(GetByIdAsync)} action {ex}");
+                return null;
+            }
         }
 
         public async Task<bool> RemoveAsync(Guid Id)
         {
-            var workPlace = await _unitOfWork.WorkPlaces.GetByIdAsync(Id);
-            if (workPlace == null)
-                return false;
+            try
+            {
+                var workPlace = await _unitOfWork.WorkPlaces.GetByIdAsync(Id);
+                if (workPlace == null)
+                    return false;
 
-            _unitOfWork.WorkPlaces.Remove(workPlace);
-            await _unitOfWork.CompleteAsync();
-            return true;
+                _unitOfWork.WorkPlaces.Remove(workPlace);
+                await _unitOfWork.CompleteAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarn($"Non correct values in the {nameof(RemoveAsync)} action {ex}");
+                return false;
+            }
         }
 
         public async Task<PagedList<GetWorkPlaceDTO>> SearchByMapIdAsync(Guid mapId, PagedQueryBase query)
         {
-            var workPlaces = await _unitOfWork.WorkPlaces.GetPagedByMapIdAsync(mapId, query);
-            var workPlaceMaps = _mapper.Map<List<GetWorkPlaceDTO>>(workPlaces);
-            var workPlacesDTO = new PagedList<GetWorkPlaceDTO>(workPlaceMaps, workPlaces.TotalCount, workPlaces.CurrentPage, workPlaces.PageSize);
-            return workPlacesDTO;
+            try
+            {
+                var workPlaces = await _unitOfWork.WorkPlaces.GetPagedByMapIdAsync(mapId, query);
+                var workPlaceMaps = _mapper.Map<List<GetWorkPlaceDTO>>(workPlaces);
+                var workPlacesDTO = new PagedList<GetWorkPlaceDTO>(workPlaceMaps, workPlaces.TotalCount, workPlaces.CurrentPage, workPlaces.PageSize);
+                return workPlacesDTO;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarn($"Non correct values in the {nameof(SearchByMapIdAsync)} action {ex}");
+                return null;
+            }
         }
 
         public async Task<GetWorkPlaceDTO> UpdateAsync(UpdateWorkPlaceDTO workPlaceDTO)
         {
-            var workPlace = await _unitOfWork.WorkPlaces.GetByIdAsync(workPlaceDTO.Id);
-            if (workPlace == null)
-                return null;
+            try
+            {
+                var workPlace = await _unitOfWork.WorkPlaces.GetByIdAsync(workPlaceDTO.Id);
+                if (workPlace == null)
+                    return null;
 
-            _mapper.Map(workPlaceDTO, workPlace);
-            _unitOfWork.WorkPlaces.Update(workPlace);
-            await _unitOfWork.CompleteAsync();
-            return _mapper.Map<GetWorkPlaceDTO>(workPlace);
+                _mapper.Map(workPlaceDTO, workPlace);
+                _unitOfWork.WorkPlaces.Update(workPlace);
+                await _unitOfWork.CompleteAsync();
+                return _mapper.Map<GetWorkPlaceDTO>(workPlace);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarn($"Non correct values in the {nameof(UpdateAsync)} action {ex}");
+                return null;
+            }
         }
     }
 }
