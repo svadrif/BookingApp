1373d439d88255e72fc4fcfb0a863241e32e3d99
temshik <hotko.artem2901@gmail.com>
2022-03-24 15:27:15 +0300

    Edit Conflicts

---
+++ src/API/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Application.DTOs.AppUserDTO;
+using Application.Interfaces;
+using Application.Interfaces.IServices;
+using Application.Pagination;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IAppUserService _appUserService;
+        private readonly IAuthenticationService _authenticationService;
+        private readonly ILoggerManager _logger;
+        public UserController(IAppUserService appUserService, IAuthenticationService authenticationService, ILoggerManager logger)
+        {
+            _appUserService = appUserService;
+            _authenticationService = authenticationService;
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("SignIn")]
+        public async Task<ActionResult> SignIn([FromBody] Guid id)
+        {
+            var authResult = await _authenticationService.AuthenticateAsync(id);
+            _logger.LogInfo($"{nameof(SignIn)} method by {id}");
+            if (authResult == null)
+                return Unauthorized();
+
+            return Ok(authResult);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("SignUp")]
+        public async Task<IActionResult> SignUp([FromBody] AddAppUserDTO newUser)
+        {
+            var userId = await _appUserService.AddAsync(newUser);
+            var authResult = await _authenticationService.AuthenticateAsync(userId);
+            _logger.LogInfo($"called method {nameof(SignUp)} by {userId}");
+            return Ok(authResult);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> GetUsersPaged([FromQuery] PagedQueryBase query)
+        {
+            var users = await _appUserService.GetPagedAsync(query);
+            _logger.LogInfo($"called method {nameof(GetUsersPaged)}");
+            return Ok(users);
+        }
+    }
+}
---
+++ src/Infrastructure/Services/AppUserService.cs
@@ -0,0 +1,83 @@
+﻿using Application.DTOs.AppUserDTO;
+using Application.Interfaces.IRepositories;
+using Application.Interfaces.IServices;
+using Application.Pagination;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Infrastructure.Services
+{
+    public class AppUserService : IAppUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IStateService _stateService;
+        private readonly IBookingHistoryService _historyService;
+
+        public AppUserService(IUnitOfWork unitOfWork, IMapper mapper, IStateService stateService, IBookingHistoryService historyService)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _stateService = stateService;
+            _historyService = historyService;
+        }
+
+        public async Task<Guid> AddAsync(AddAppUserDTO appUserDTO)
+        {
+            AppUser appUser = _mapper.Map<AppUser>(appUserDTO);
+            await _unitOfWork.AppUsers.AddAsync(appUser);
+            await _unitOfWork.CompleteAsync();
+
+            await _stateService.AddAsync(new State() { UserId = appUser.Id });
+            await _historyService.AddAsync(new BookingHistory() { UserId = appUser.Id });
+
+            return appUser.Id;
+        }
+
+        public async Task<PagedList<GetAppUserDTO>> GetPagedAsync(PagedQueryBase query)
+        {
+            var appUsers = await _unitOfWork.AppUsers.GetPagedAsync(query);
+            var mapUsers = _mapper.Map<List<GetAppUserDTO>>(appUsers);
+            var appUsersDTO = new PagedList<GetAppUserDTO>(mapUsers, appUsers.TotalCount, appUsers.CurrentPage, appUsers.PageSize);
+            return appUsersDTO;
+        }
+
+        public async Task<GetAppUserDTO> GetByIdAsync(Guid Id)
+        {
+            var appUsers = await _unitOfWork.AppUsers.GetByIdAsync(Id);
+            return _mapper.Map<GetAppUserDTO>(appUsers);
+        }
+
+        public async Task<GetAppUserDTO> GetByTelegramIdAsync(long telegramId)
+        {
+            var appUsers = await _unitOfWork.AppUsers.GetByTelegramIdAsync(telegramId);
+            return _mapper.Map<GetAppUserDTO>(appUsers);
+        }
+
+        public async Task<bool> RemoveAsync(Guid Id)
+        {
+            var appUser = await _unitOfWork.AppUsers.GetByIdAsync(Id);
+            if (appUser == null)
+                return false;
+
+            await _stateService.RemoveByUserIdAsync(Id);
+            await _historyService.RemoveByUserIdAsync(Id);
+
+            _unitOfWork.AppUsers.Remove(appUser);
+            await _unitOfWork.CompleteAsync();
+            return true;
+        }
+
+        public async Task<GetAppUserDTO> UpdateAsync(UpdateAppUserDTO appUserDTO)
+        {
+            var appUser = await _unitOfWork.AppUsers.GetByIdAsync(appUserDTO.Id);
+            if (appUser == null)
+                return null;
+
+            _mapper.Map(appUserDTO, appUser);
+            _unitOfWork.AppUsers.Update(appUser);
+            await _unitOfWork.CompleteAsync();
+            return _mapper.Map<GetAppUserDTO>(appUser);
+        }
+    }
+}
